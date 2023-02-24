f05fd35d92b075f62aab4b149db4a0fcf7317427
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-02-24 09:52:53 +0100

    Add files via upload

---
+++ cw_1.py
@@ -0,0 +1,40 @@
+print("Zad 1")
+a = "*" * 18
+print((a + "\n") * 4)
+print("Zad 2")
+b = " "*14
+print(a)
+print("*",b, "*")
+print("*",b, "*")
+print(a)
+
+print("Zad 3")
+h = "*"
+for i in range(1, 5):
+    print(h * i)
+
+print("Zad 4")
+print((512-282)/(47*48+5))
+
+print("Zad 5")
+x = 7
+print(str(x) + "---" + str(x*2) + "---" + str(x*3) + "---" + str(x*4) + "---" + str(x*5))
+
+print("Zad 6")
+string = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+print(string)
+
+print("Zad 7")
+c = eval(input("Enter the first number : "))
+d = eval(input("Enter the second number :"))
+print("Result is ", c*d)
+
+print("Zad 8")
+kg = eval(input("Enter the weigth in kg :"))
+print(kg*2.2)
+
+
+
+
+
+
