8113a05a7ed597b40c64a80c5149b6c86d30383e
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-03 10:57:56 +0100

    Update zadanie_8.py

--- cw_2.py/zadanie_8.py
+++ cw_2.py/zadanie_8.py
@@ -1,6 +1,6 @@
-number = int(input("Please, enter the number, n = "))
-suma = 0
-digit = int(number % 10)
-for digit in number:
-    sum += digit
-    print(suma)
+number = eval(input("Please, enter the number, n = "))
+sum = 0
+for i in str(number):
+    sum += int(i)
+
+print("The sum is ", sum)
