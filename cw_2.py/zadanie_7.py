a4b61f8bf160a78d06bf12ad2708885994f07c84
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-03 10:56:33 +0100

    Update zadanie_7.py

--- cw_2.py/zadanie_7.py
+++ cw_2.py/zadanie_7.py
@@ -1,3 +1,6 @@
-x = input("Please, enter the numbers : " )
-lista = [x]
-for x in range()
+number = eval(input("Please, enter the number, n = "))
+sum = 0
+for i in str(number):
+    sum += int(i)
+
+print("The sum is ", sum)
