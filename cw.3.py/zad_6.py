fca5b4c91bef1c3f41f810a4af046344e177806d
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-13 18:50:30 +0100

    Update zad_6.py

--- cw.3.py/zad_6.py
+++ cw.3.py/zad_6.py
@@ -3,7 +3,7 @@ import random
 
 def guess_the_number(number):
     skore = 0
-    random_number = random.randint(1, 11)
+    random_number = random.randint(1, 10)
     for i in range(1, 6):
         print(input("Please, enter the number"))
         print("Random number is ", random_number)
