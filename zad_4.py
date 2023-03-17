21db08bbb895ffc82dff3784aa5e8fcc921e5e2b
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-17 10:32:24 +0100

    Delete zad_4.py

--- zad_4.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def potegowanie(a, n):
-    powers = np.logspace(1, n, num=n, base=a, dtype="int64")
-    return powers
-a = eval(input("Please, enter the number n = "))
-n = eval(input("Please, enter the number k = "))
-
-print(potegowanie(a, n))
