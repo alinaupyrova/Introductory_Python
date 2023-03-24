abdf6bae89400d485a472e72d7be9b60e997ff19
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-24 09:00:16 +0100

    Update zad_6.py

--- cw_4.py/zad_6.py
+++ cw_4.py/zad_6.py
@@ -7,7 +7,6 @@ def foo6():
     third_word_3 = third_word[::-1]
     print(third_word_3)
     matrix = np.zeros((6, 6), dtype=str)
-   # matrix[:, 0] = np.array(list(first_word))
     matrix[:, 0] = np.fromiter(first_word, dtype="S1")
     matrix[5:, ::-1] = np.fromiter(third_word, dtype="S2")
     np.fill_diagonal(matrix, list(second_word))
