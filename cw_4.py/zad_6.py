5e0a294d7bfb1aef589870aeb19495897479d9ba
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-24 08:59:42 +0100

    Add files via upload

---
+++ cw_4.py/zad_6.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def foo6():
+    first_word = "malina"
+    second_word = "lizak"
+    third_word = "jagoda"
+    third_word_3 = third_word[::-1]
+    print(third_word_3)
+    matrix = np.zeros((6, 6), dtype=str)
+   # matrix[:, 0] = np.array(list(first_word))
+    matrix[:, 0] = np.fromiter(first_word, dtype="S1")
+    matrix[5:, ::-1] = np.fromiter(third_word, dtype="S2")
+    np.fill_diagonal(matrix, list(second_word))
+    print(matrix)
+
+foo6()
