636979e13654cd8b4bc7e17daeea7a96517d3495
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-30 16:06:08 +0200

    Update zadanie_1.py

--- cw_2.py/zadanie_1.py
+++ cw_2.py/zadanie_1.py
@@ -1,3 +1,12 @@
-
+first case :
 new_var = input("Please, enter some text :")
 print(new_var.count(" "))
+
+second case:
+word = input("Please, enter the some words")
+counter = 0
+for i in range (word.__len__()):
+    if word[i] == ' ':
+       counter += 1
+
+print(counter)
