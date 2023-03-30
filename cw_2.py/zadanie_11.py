edfb11e77928fd01c0981ac3b30a5c5c0952cd08
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-30 17:21:35 +0200

    Update zadanie_11.py

--- cw_2.py/zadanie_11.py
+++ cw_2.py/zadanie_11.py
@@ -1,8 +1,8 @@
-h = int(input("Please, enter the height of your diamond, h =  "))
-if h < 3 or h > 9:
-    h = input("Please, enter the height again, h = ")
-for i in h:
-    for j in h - 1:
-        if h == 2:
-            print("o")
+h = int(input("Please, enter the height (3, 9): "))
+if h < 3 and h > 9:
+    h = int(input("Please, enter again the height (3, 9): "))
+for i in range(1, h + 1, 2):
+    print(" " * ((h - i) // 2) + "o" * i)
+for i in range(h-2, 0, -2):
+    print(" " * ((h - i) // 2) + "o" * i)
 
