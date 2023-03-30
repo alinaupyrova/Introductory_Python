996a337add6455fd4731a29e37b397b965ec374e
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-30 17:05:36 +0200

    Update zadanie_10.py

--- cw_2.py/zadanie_10.py
+++ cw_2.py/zadanie_10.py
@@ -2,5 +2,5 @@ h = int(input("Please, enter the height of your pyramid, h =  "))
 while h > 10:
     h = input("Please, enter the height again, h = ")
 a = "A"
-for i in range(1, h):
+for i in range(1, h + 1):
     print(i * a)
