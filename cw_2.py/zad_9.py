2a94465ea75bff6c398c00a0a78fc771c0d556d4
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-30 17:53:19 +0300

    Add files via upload

---
+++ cw_2.py/zad_9.py
@@ -0,0 +1,12 @@
+lista = []
+while True:
+    a = input("Please, enter the number or end: ")
+    if a == 'end':
+        break
+    elif a.isdigit():
+        number = int(a)
+        lista.append(number)
+        print("You add the number", number)
+    else:
+        print("error,enter the number or end")
+print(lista)
