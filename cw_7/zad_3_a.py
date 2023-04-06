8b9fb156a87af2249e8edbb0f9acfaeaf7cf79af
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-06 22:50:11 +0200

    Update zad_3_a.py

--- cw_7/zad_3_a.py
+++ cw_7/zad_3_a.py
@@ -1,9 +1,4 @@
 import pandas as pd
-import xlrd
-import openpyxl
-
-zamowienia = pd.read_csv("zamowienia.csv")
-print(zamowienia)
-
-a = zamowienia['Sprzedawca'].unique().tolist()
-print(a)
+df = pd.read_csv("zamowienia.csv", delimiter=";")
+unique_sellers = df["Sprzedawca"].unique()
+print(unique_sellers)
