1c65a7ea54a9f7bb453d8eab5bca49fbf11ee132
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-09 23:02:08 +0300

    Add files via upload

---
+++ cw_7(07.04)/zad_1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# загрузить данные из файла Excel
+data = pd.read_excel('imiona.xlsx')
+
+# сгруппировать данные по году и посчитать общее количество родившихся детей в каждом году
+births_by_year = data.groupby('Rok').sum()['Liczba']
+
+# создать линейный график
+plt.plot(births_by_year.index, births_by_year.values)
+
+# добавить подписи осей и заголовок графика
+plt.xlabel('Year')
+plt.ylabel('Number of births')
+plt.title('Number of births by year')
+
+# показать график
+plt.show()
