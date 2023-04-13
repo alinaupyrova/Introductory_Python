880e23687916fc106e6d1d2f2eaf034eeda32c81
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-13 09:02:55 +0200

    Update zad_2.py

--- cw_4.py/zad_2.py
+++ cw_4.py/zad_2.py
@@ -4,3 +4,11 @@ float_lista = np.array(float_lista)
 int_lista = float_lista.astype(np.int64)
 print("Float array : ", float_lista)
 print("Int array : ", int_lista)
+
+OR 
+
+lista = [1.4, 5.6, 7.9]
+a = np.array(lista)
+b = np.array(lista, dtype='int64')
+print(a)
+print(b)
