de93acb4edd51cbd5a82138de092cf06baa1655c
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-13 09:14:23 +0200

    Update zad_3.py

--- cw_4.py/zad_3.py
+++ cw_4.py/zad_3.py
@@ -1,5 +1,8 @@
 import numpy as np
-n = eval(input("Please, enter the number n = "))
-arr = np.arange(1, n*n+1)
+def arr_create(n):
+    arr = np.arange(1, n * n + 1)
+    return arr
+n = int(input("Please, enter the number n = "))
+arr = arr_create(n)
 print("Array : ", arr)
 print("Size of array is ", arr.shape)
