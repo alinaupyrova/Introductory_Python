ae79c81b2757c19fe7cfff2ca42b8e7e9772ee53
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-13 11:26:43 +0200

    Update zad_5.py

--- cw_4.py/zad_5.py
+++ cw_4.py/zad_5.py
@@ -5,3 +5,14 @@ def function(length_of_vector):
     diagonal = np.diag([i for i in arr], 0)
     print(diagonal)
     function(4)
+    
+    np.arange([start, ]stop, [step, ]dtype=None)
+    
+    import numpy as np
+    n = int(input("n = "))
+    def foo(n):
+
+    wec = np.arange(n, 0, -1)
+    diagonal = np.diag(wec)
+    print(diagonal)
+print(foo(n))
