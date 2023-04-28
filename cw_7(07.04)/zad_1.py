4ae4f4db7039e9ee417bb922fc7570d36aac3c6f
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-28 10:06:32 +0200

    Update zad_1.py

--- cw_7(07.04)/zad_1.py
+++ cw_7(07.04)/zad_1.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 data = pd.read_excel('imiona.xlsx')
 
 # сгруппировать данные по году и посчитать общее количество родившихся детей в каждом году
-births_by_year = data.groupby('Rok').sum()['Liczba']
-
+births_by_year = data.groupby('Rok')['Liczba'].sum()
+births_by_year = data.groupby('Rok').sum()['Liczba'] 
 # создать линейный график
 plt.plot(births_by_year.index, births_by_year.values)
 
